302547b03190e690e9844a27b1d6969fae3a5a8d
evil-kekc <3371824@gmail.com>
2023-07-06 20:31:06 +0300

    Add PasswordReset forms

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
 
-    # login urls
+    # login-logout urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
@@ -16,6 +16,20 @@ urlpatterns = [
     path('password-change/done/',
          auth_views.PasswordChangeDoneView.as_view(),
          name='password_change_done'),
+
+    # password reset urls
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 
     path('', views.dashboard, name='dashboard'),
 ]
