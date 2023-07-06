a1396d952121875c8541e2d1648bc714b87da752
evil-kekc <3371824@gmail.com>
2023-07-06 19:54:27 +0300

    Add PasswordChange form

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,17 @@ from . import views
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
 
+    # login urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # password change urls
+    path('password-change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password-change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+
     path('', views.dashboard, name='dashboard'),
 ]
