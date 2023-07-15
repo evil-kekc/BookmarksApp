a8671f48d4f92501f31f35755c570454cf366e2b
evil-kekc <3371824@gmail.com>
2023-07-15 22:50:12 +0300

    Add UserRegistrationForm

--- account/urls.py
+++ account/urls.py
@@ -31,5 +31,7 @@ urlpatterns = [
          auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
 
+    path('', include('django.contrib.auth.urls')),
+
     path('', views.dashboard, name='dashboard'),
 ]
